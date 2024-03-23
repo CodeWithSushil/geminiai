3bb148c89e84ab8b5333919037f6736a11ad7d63
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-03-23 21:49:24 +0530

    dev 0.0.01

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
-require_once("vendor/autoload.php");
+define("APP_ROOT", __DIR__);
+
+require_once APP_ROOT . "/vendor/autoload.php";
+
+// autoloader for namespaced classes
+spl_autoload_register(function($class)
+{
+  $classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class .".php");
+  $classPath = APP_ROOT ."/app/". $classFile;
+  
+  if (file_exists($classPath))
+  {
+    require_once $classPath;
+  }
+});
+
+session_start();
+
+use App\Services\Route;
+
+$route = new Route();
+require_once APP_ROOT ."/routes/route.php";
+$route->handle();
+
+
+/*require_once("vendor/autoload.php");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -9,4 +34,4 @@ $client = Gemini::client($yourApiKey);
 $result = $client->geminiPro()->generateContent('Hello');
 echo "<b>Client: </b>Hello?<br/>";
 echo "<b>Gemini: </b>". $result->text(); // Hello! How can I assist you today?
-
+*/
---
+++ routes/route.php
@@ -0,0 +1,13 @@
+<?php
+use App\Services\Route;
+use App\Middleware\{Auth, Guest};
+
+Route::get("login", "LoginController", "index", [Guest::class]);
+Route::get("register", "RegisterController", "index", [Guest::class]);
+
+Route::post("submit-register", "REgisterController", "register", [Guest::class]);
+Route::post("submit-login", "LoginController", "login", [Guest::class]);
+
+Route::get("logout", "DashboardController", "logout", [Auth::class]);
+Route::get("dashboard", "DashboardController", "index", [Auth::class]);
+Route::get("", "HomeController", "index");
