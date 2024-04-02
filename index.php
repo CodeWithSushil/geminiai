215260568d856f13ff9d80a13c3425300dfc2364
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-04-02 20:43:52 +0530

    testing

--- index.php
+++ index.php
@@ -6,7 +6,21 @@ $dotenv->load();
 
 $yourApiKey = $_ENV['API_KEY'];
 $client = Gemini::client($yourApiKey);
-$result = $client->geminiPro()->generateContent('Hello');
-echo "<b>Client: </b>Hello?<br/>";
-echo "<b>Gemini: </b>". $result->text(); // Hello! How can I assist you today?
 
+if(isset($_POST['submit'])){
+  $q = $_POST['text'];
+  $result = $client->geminiPro()->generateContent($q);
+  
+  echo "<b>Client: </b>$q?<br/>";
+  echo "<b>Gemini: </b>". $result->text();
+}else {
+  $result = $client
+    ->geminiPro()
+    ->generateContent('Hello');
+  echo "<b>Gemini: </b>". $result->text();
+}
+?>
+<form method="POST" action="index.php">
+<input name="text" placeholder="Query">
+<button type="submit" name="submit">Submit</button>
+</form>
